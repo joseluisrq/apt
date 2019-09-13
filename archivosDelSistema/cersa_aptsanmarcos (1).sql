0cc8c6a7b1c51a01af5f63d0bf371eb0d33061de
Jose Luis Ramirez <joseramirezq.info@gmail.com>
2019-09-13 18:47:19 -0500

    base de datos

--- archivosDelSistema/cersa_aptsanmarcos (1).sql
+++ archivosDelSistema/cersa_aptsanmarcos (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-09-2019 a las 01:49:15
+-- Tiempo de generación: 14-09-2019 a las 01:46:26
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 7.3.8
 
@@ -50,7 +50,10 @@ INSERT INTO `clientes` (`id`, `estado`, `estadocredito`) VALUES
 (43, '1', '1'),
 (44, '1', '1'),
 (45, '1', '0'),
-(46, '1', '0');
+(46, '0', '1'),
+(48, '0', '0'),
+(49, '0', '0'),
+(51, '0', '0');
 
 -- --------------------------------------------------------
 
@@ -80,27 +83,27 @@ CREATE TABLE `creditos` (
 --
 
 INSERT INTO `creditos` (`id`, `idcliente`, `idusuario`, `numeroprestamo`, `idkiva`, `montodesembolsado`, `fechadesembolso`, `numerocuotas`, `tipocambio`, `tasa`, `estado`, `periodo`, `created_at`, `updated_at`) VALUES
-(49, 14, 13, 'SM20150001', '847553', '1700.00', '2015-05-25', 12, '3.150000', '13.00', '1', '1', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
-(50, 15, 13, 'SM20150002', '847562', '1700.00', '2015-05-25', 12, '3.150000', '11.00', '2', '1', '2019-09-03 08:37:40', '2019-09-03 08:37:40'),
-(51, 16, 13, 'SM20150003', '847598', '1700.00', '2015-05-25', 12, '3.150000', '11.00', '1', '1', '2019-09-03 08:47:33', '2019-09-03 08:47:33'),
-(52, 37, 13, '45', '45', '1200.00', '2019-09-14', 4, '3.150000', '13.00', '1', '1', '2019-09-09 21:53:03', '2019-09-09 21:53:03'),
-(53, 18, 13, 'SM00005854', '1245325', '1800.00', '2019-09-11', 4, '3.150000', '11.00', '1', '1', '2019-09-11 22:24:20', '2019-09-11 22:24:20'),
-(54, 17, 13, 'SM0058', '1232654', '1700.00', '2019-09-11', 12, '3.250000', '11.00', '1', '1', '2019-09-12 02:49:14', '2019-09-12 02:49:14'),
-(55, 41, 13, 'aaaasdas45', 'asdasd4564', '1700.00', '2019-09-11', 12, '3.150000', '11.00', '1', '1', '2019-09-12 03:21:15', '2019-09-12 03:21:15'),
-(56, 43, 13, 'asdasd45', 'asdasd', '1200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', '2019-09-12 04:53:40', '2019-09-12 04:53:40'),
-(57, 44, 13, 'sm0000', 'ki00045', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', '2019-09-13 01:31:17', '2019-09-13 01:31:17');
+(49, 14, 13, 'SM20150001', '847553', '1800.00', '2015-05-25', 12, '3.150000', '13.00', '1', '1', '2019-09-02 06:48:36', '2019-09-14 02:51:41'),
+(50, 15, 13, 'SM20150002', '847562', '1700.00', '2015-05-25', 12, '3.150000', '11.00', '1', '1', '2019-09-03 08:37:40', '2019-09-14 01:17:03'),
+(51, 16, 13, 'SM20150003', '847598', '17800.00', '2015-05-25', 12, '3.150000', '11.00', '1', '1', '2019-09-03 08:47:33', '2019-09-14 03:00:53'),
+(52, 37, 13, '4553', '4553', '1152.00', '2019-09-14', 52, '3.150000', '13.00', '1', '1', '2019-09-09 21:53:03', '2019-09-14 03:01:30'),
+(53, 18, 13, 'SM00005854', '1245325', '1800.00', '2019-09-11', 4, '3.150000', '11.00', '1', '1', '2019-09-11 22:24:20', '2019-09-13 22:22:29'),
+(54, 17, 13, 'SM005854', '1232654', '1700.00', '2019-09-11', 12, '3.250000', '11.00', '1', '1', '2019-09-12 02:49:14', '2019-09-14 01:56:50'),
+(55, 41, 13, 'aaaasdas45', 'asdasd4564', '1700.00', '2019-09-11', 12, '3.150000', '11.00', '1', '1', '2019-09-12 03:21:15', '2019-09-13 22:21:59'),
+(56, 43, 13, 'asdasd45', 'asdasd', '15200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', '2019-09-12 04:53:40', '2019-09-14 02:51:31'),
+(57, 44, 13, 'pescado', 'ki00045f', '12200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', '2019-09-13 01:31:17', '2019-09-14 03:32:10');
 
 --
 -- Disparadores `creditos`
 --
 DELIMITER $$
-CREATE TRIGGER `actualizarcredito` AFTER UPDATE ON `creditos` FOR EACH ROW INSERT INTO creditos_bitacora(idcliente_n,idusuario_n,numeroprestamo_n,idkiva_n,montodesembolsado_n,fechadesembolso_n,numerocuotas_n,tipocambio_n,tasa_n,estado_n,periodo_n,idcliente_v,idusuario_v,numeroprestamo_v,idkiva_v,montodesembolsado_v,fechadesembolso_v,numerocuotas_v,tipocambio_v,tasa_v,estado_v,periodo_v,condicion)
-VALUES(NEW.idcliente,NEW.idusuario,NEW.numeroprestamo,NEW.idkiva,NEW.montodesembolsado,NEW.fechadesembolso,NEW.numerocuotas,NEW.tipocambio,NEW.tasa,NEW.estado,NEW.periodo,old.idcliente,old.idusuario,old.numeroprestamo,old.idkiva,old.montodesembolsado,old.fechadesembolso,old.numerocuotas,old.tipocambio,old.tasa,old.estado,old.periodo,'CREDITO ACTUALIZADO')
+CREATE TRIGGER `actualizarcredito` AFTER UPDATE ON `creditos` FOR EACH ROW INSERT INTO creditos_bitacora(idcredito,idcliente_n,idusuario_n,numeroprestamo_n,idkiva_n,montodesembolsado_n,fechadesembolso_n,numerocuotas_n,tipocambio_n,tasa_n,estado_n,periodo_n,idcliente_v,idusuario_v,numeroprestamo_v,idkiva_v,montodesembolsado_v,fechadesembolso_v,numerocuotas_v,tipocambio_v,tasa_v,estado_v,periodo_v,condicion)
+VALUES(old.id,NEW.idcliente,NEW.idusuario,NEW.numeroprestamo,NEW.idkiva,NEW.montodesembolsado,NEW.fechadesembolso,NEW.numerocuotas,NEW.tipocambio,NEW.tasa,NEW.estado,NEW.periodo,old.idcliente,old.idusuario,old.numeroprestamo,old.idkiva,old.montodesembolsado,old.fechadesembolso,old.numerocuotas,old.tipocambio,old.tasa,old.estado,old.periodo,'CREDITO ACTUALIZADO')
 $$
 DELIMITER ;
 DELIMITER $$
-CREATE TRIGGER `insertarcredito` AFTER INSERT ON `creditos` FOR EACH ROW INSERT INTO creditos_bitacora(idcliente_n,idusuario_n,numeroprestamo_n,idkiva_n,montodesembolsado_n,fechadesembolso_n,numerocuotas_n,tipocambio_n,tasa_n,estado_n,periodo_n,condicion)
-VALUES(NEW.idcliente,NEW.idusuario,NEW.numeroprestamo,NEW.idkiva,NEW.montodesembolsado,NEW.fechadesembolso,NEW.numerocuotas,NEW.tipocambio,NEW.tasa,NEW.estado,NEW.periodo,'NUEVO CREDITO')
+CREATE TRIGGER `insertarcredito` AFTER INSERT ON `creditos` FOR EACH ROW INSERT INTO creditos_bitacora(idcredito,idcliente_n,idusuario_n,numeroprestamo_n,idkiva_n,montodesembolsado_n,fechadesembolso_n,numerocuotas_n,tipocambio_n,tasa_n,estado_n,periodo_n,condicion)
+VALUES(NEW.id,NEW.idcliente,NEW.idusuario,NEW.numeroprestamo,NEW.idkiva,NEW.montodesembolsado,NEW.fechadesembolso,NEW.numerocuotas,NEW.tipocambio,NEW.tasa,NEW.estado,NEW.periodo,'NUEVO CREDITO')
 $$
 DELIMITER ;
 
@@ -112,6 +115,7 @@ DELIMITER ;
 
 CREATE TABLE `creditos_bitacora` (
   `id` int(10) UNSIGNED NOT NULL,
+  `idcredito` int(10) NOT NULL,
   `idcliente_n` int(10) NOT NULL,
   `idcliente_v` int(10) NOT NULL,
   `idusuario_n` int(10) NOT NULL,
@@ -142,10 +146,15 @@ CREATE TABLE `creditos_bitacora` (
 -- Volcado de datos para la tabla `creditos_bitacora`
 --
 
-INSERT INTO `creditos_bitacora` (`id`, `idcliente_n`, `idcliente_v`, `idusuario_n`, `idusuario_v`, `numeroprestamo_n`, `idkiva_n`, `montodesembolsado_n`, `fechadesembolso_n`, `numerocuotas_n`, `tipocambio_n`, `tasa_n`, `estado_n`, `periodo_n`, `numeroprestamo_v`, `idkiva_v`, `montodesembolsado_v`, `fechadesembolso_v`, `numerocuotas_v`, `tipocambio_v`, `tasa_v`, `estado_v`, `periodo_v`, `condicion`, `fechacambio`) VALUES
-(0, 44, 0, 13, 0, 'sm0000', 'ki000', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', '', '', '0.00', '0000-00-00', 0, '0.000000', '0.00', '', '', 'NUEVO CREDIT', '2019-09-12 20:31:17'),
-(0, 44, 44, 13, 13, 'sm0000', 'ki00045', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'sm0000', 'ki000', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-12 20:45:38'),
-(0, 43, 43, 13, 13, 'asdasd45', 'asdasd', '1200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', 'asdasd', 'asdasd', '1200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', 'CREDITO ACTUALIZADO', '2019-09-12 20:46:02');
+INSERT INTO `creditos_bitacora` (`id`, `idcredito`, `idcliente_n`, `idcliente_v`, `idusuario_n`, `idusuario_v`, `numeroprestamo_n`, `idkiva_n`, `montodesembolsado_n`, `fechadesembolso_n`, `numerocuotas_n`, `tipocambio_n`, `tasa_n`, `estado_n`, `periodo_n`, `numeroprestamo_v`, `idkiva_v`, `montodesembolsado_v`, `fechadesembolso_v`, `numerocuotas_v`, `tipocambio_v`, `tasa_v`, `estado_v`, `periodo_v`, `condicion`, `fechacambio`) VALUES
+(1, 57, 44, 44, 13, 13, 'sm0000d', 'ki00045', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'sm0000', 'ki00045', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 21:23:24'),
+(2, 57, 44, 44, 13, 13, 'sm0000d', 'ki00045f', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'sm0000d', 'ki00045', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 21:23:28'),
+(3, 57, 44, 44, 13, 13, 'sm0000d', 'ki00045f', '12200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'sm0000d', 'ki00045f', '1200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 21:50:50'),
+(4, 56, 43, 43, 13, 13, 'asdasd45', 'asdasd', '15200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', 'asdasd45', 'asdasd', '1200.00', '2019-09-11', 4, '3.150000', '11.00', '1', '3', 'CREDITO ACTUALIZADO', '2019-09-13 21:51:31'),
+(5, 49, 14, 14, 13, 13, 'SM20150001', '847553', '1800.00', '2015-05-25', 12, '3.150000', '13.00', '1', '1', 'SM20150001', '847553', '1700.00', '2015-05-25', 12, '3.150000', '13.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 21:51:41'),
+(7, 51, 16, 16, 13, 13, 'SM20150003', '847598', '17800.00', '2015-05-25', 12, '3.150000', '11.00', '1', '1', 'SM20150003', '847598', '1700.00', '2015-05-25', 12, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 22:00:53'),
+(8, 52, 37, 37, 13, 13, '4553', '4553', '1152.00', '2019-09-14', 52, '3.150000', '13.00', '1', '1', '4553', '4553', '1252.00', '2019-09-14', 52, '3.150000', '13.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 22:01:30'),
+(9, 57, 44, 44, 13, 13, 'pescado', 'ki00045f', '12200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'sm0000d', 'ki00045f', '12200.00', '2019-09-12', 4, '3.150000', '11.00', '1', '1', 'CREDITO ACTUALIZADO', '2019-09-13 22:32:10');
 
 -- --------------------------------------------------------
 
@@ -174,7 +183,7 @@ CREATE TABLE `cuotas` (
 --
 
 INSERT INTO `cuotas` (`id`, `idusuario`, `idcredito`, `numerocuota`, `fechapago`, `fechacancelacion`, `saldopendiente`, `monto`, `otroscostos`, `descripcion`, `estado`, `created_at`, `updated_at`) VALUES
-(147, 13, 49, 0, '2015-06-25', '2019-09-01 00:00:00', '1558.33', '157.25', '0.00', NULL, '0', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
+(147, 13, 49, 0, '2015-06-25', '2019-09-01 00:00:00', '1553.33', '158.25', '0.00', NULL, '0', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
 (148, 13, 49, 0, '2015-07-25', '2019-09-01 00:00:00', '1416.66', '157.25', '0.00', NULL, '0', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
 (149, 13, 49, 0, '2015-08-25', '2019-09-01 00:00:00', '1274.99', '157.25', '0.00', NULL, '0', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
 (150, 13, 49, 0, '2015-09-25', '2019-09-01 00:00:00', '1133.32', '157.25', '0.00', NULL, '0', '2019-09-02 06:48:36', '2019-09-02 06:48:36'),
@@ -250,6 +259,51 @@ INSERT INTO `cuotas` (`id`, `idusuario`, `idcredito`, `numerocuota`, `fechapago`
 (220, 13, 57, 2, '2019-11-12', '2019-09-12 00:00:00', '600.00', '333.00', '0.00', NULL, '0', '2019-09-13 01:31:17', '2019-09-13 01:31:17'),
 (221, 13, 57, 3, '2019-12-12', '2019-09-12 00:00:00', '300.00', '333.00', '0.00', NULL, '0', '2019-09-13 01:31:17', '2019-09-13 01:31:17'),
 (222, 13, 57, 4, '2020-01-12', '2019-09-12 00:00:00', '0.00', '333.00', '0.00', NULL, '0', '2019-09-13 01:31:17', '2019-09-13 01:31:17');
+
+--
+-- Disparadores `cuotas`
+--
+DELIMITER $$
+CREATE TRIGGER `actualizarcuota` AFTER UPDATE ON `cuotas` FOR EACH ROW INSERT INTO
+cuotas_bitacora(idcuota,idusuario,idusuario_n,idcredito,numerocuota,numerocuota_n,fechapago,fechapago_n,saldopendiente,saldopendiente_n,monto,monto_n,otroscostos,otroscostos_n,descripcion,descripcion_n,estado,estado_n)
+VALUES(old.id,old.idusuario,NEW.idusuario,old.idcredito,old.numerocuota,NEW.numerocuota,old.fechapago,NEW.fechapago,old.saldopendiente,NEW.saldopendiente,old.monto,NEW.monto,old.otroscostos,NEW.otroscostos,old.descripcion,NEW.descripcion,old.estado,NEW.estado)
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuotas_bitacora`
+--
+
+CREATE TABLE `cuotas_bitacora` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `idcuota` int(10) NOT NULL,
+  `idusuario` int(10) NOT NULL,
+  `idcredito` int(10) NOT NULL,
+  `numerocuota` int(2) NOT NULL,
+  `fechapago` date NOT NULL,
+  `saldopendiente` decimal(12,2) NOT NULL,
+  `monto` decimal(7,2) NOT NULL,
+  `otroscostos` decimal(7,2) DEFAULT NULL,
+  `descripcion` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estado` char(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '0 sin pagar / 1 pagada',
+  `idusuario_n` int(10) NOT NULL,
+  `numerocuota_n` int(2) NOT NULL,
+  `fechapago_n` date NOT NULL,
+  `saldopendiente_n` decimal(12,2) NOT NULL,
+  `monto_n` decimal(7,2) NOT NULL,
+  `otroscostos_n` decimal(7,2) DEFAULT NULL,
+  `descripcion_n` varchar(120) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estado_n` char(1) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '0 sin pagar / 1 pagada'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `cuotas_bitacora`
+--
+
+INSERT INTO `cuotas_bitacora` (`id`, `idcuota`, `idusuario`, `idcredito`, `numerocuota`, `fechapago`, `saldopendiente`, `monto`, `otroscostos`, `descripcion`, `estado`, `idusuario_n`, `numerocuota_n`, `fechapago_n`, `saldopendiente_n`, `monto_n`, `otroscostos_n`, `descripcion_n`, `estado_n`) VALUES
+(1, 147, 13, 49, 0, '2015-06-25', '1558.33', '158.25', '0.00', NULL, '0', 13, 0, '2015-06-25', '1553.33', '158.25', '0.00', NULL, '0');
 
 -- --------------------------------------------------------
 
@@ -327,11 +381,15 @@ INSERT INTO `personas` (`id`, `dni`, `nombre`, `apellidopaterno`, `apellidomater
 (37, '70121gh', 'asd', 'asd', 'asd', '2019-09-07', 'asd', 'asd', 'asd', '1', '2019-09-09 21:45:38', '2019-09-09 21:47:09'),
 (40, '8888', 'kkkkkk', 'kkkkkkkkkkkkkkkkkkkk', 'kkkkkkk', NULL, NULL, NULL, NULL, '1', '2019-09-12 03:01:18', '2019-09-12 03:01:18'),
 (41, '99999vvv', 'asd', 'asd', 'asd', NULL, NULL, NULL, NULL, '1', '2019-09-12 03:04:07', '2019-09-12 04:51:50'),
-(42, '7878', 'asdasd', 'sdfsd', 'asfsdf', '1993-03-01', 'asdfds', '1', 'sdfsdf@gmail.com', '1', '2019-09-12 04:52:42', '2019-09-12 04:52:42'),
+(42, '7878888', 'asdasd', 'sdfsd', 'asfsdf', '1993-03-01', 'asdfds', '1', 'sdfsdf@gmail.com', '1', '2019-09-12 04:52:42', '2019-09-13 22:27:04'),
 (43, '78', 'sxfsd', 'sdfsd', 'sdfsd', NULL, NULL, NULL, NULL, '1', '2019-09-12 04:53:10', '2019-09-12 04:53:10'),
 (44, '222', 'GHJ', 'GHJ', 'GHJ', NULL, NULL, NULL, NULL, '1', '2019-09-13 01:30:40', '2019-09-13 01:30:40'),
 (45, '56', '65', '565', 'SAD', NULL, NULL, NULL, NULL, '1', '2019-09-13 01:51:49', '2019-09-13 01:51:49'),
-(46, '55555', 'sad', 'asd', 'asd', NULL, NULL, NULL, NULL, '1', '2019-09-13 04:22:30', '2019-09-13 04:22:30');
+(46, '55555', 'sad', 'asd', 'asd', NULL, NULL, NULL, NULL, '1', '2019-09-13 04:22:30', '2019-09-13 04:22:30'),
+(48, '785656jjj', '45', 'hjk', 'hjk', NULL, NULL, NULL, NULL, '1', '2019-09-13 20:38:42', '2019-09-13 20:39:16'),
+(49, '822228', 'PEPE', 'DIAZ', 'CHIL', '2019-09-13', 'JR. Alfonso Ugarte', '70212065', 'asl@gmail.com', '1', '2019-09-13 20:41:01', '2019-09-13 21:32:54'),
+(51, '702120633P', 'JOSE LUIS', 'RAMIREZ', 'QUIROZS', NULL, '45821', '7620', 'GMAILCOM', '1', '2019-09-13 21:28:46', '2019-09-13 21:31:39'),
+(55, '111111', 'asdasdasd', 'asdasdasdasd', 'asdasdasd', '1993-03-01', NULL, NULL, NULL, '1', '2019-09-13 22:27:32', '2019-09-13 22:27:32');
 
 -- --------------------------------------------------------
 
@@ -394,11 +452,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `usuario`, `password`, `condicion`, `idrol`, `remember_token`) VALUES
-(42, 'a', '$2y$10$vNMW./ZdGy20psp0VFieCOyBoJqM.ElM23q/uI/aHIYMz3msZ8hrW', 1, 1, NULL),
+(42, 'a', '$2y$10$Qn5HqFFASOpEtOt8NkUxDePH9Gwng7SUNnYxasced0YrRjaS/XpVm', 1, 1, NULL),
 (13, 'admin', '$2y$10$FtoFi6GjL37k4qEvEGsFC.JsSG/cYfDMuQ8SinE/Sb6A/YwQE5hHe', 1, 1, NULL),
 (12, 'BRAVO45', '$2y$10$ehN6JcxNYcYI1uuv/DkYEObpenjLAun64qG3Lj5GnPReMH8HlOZfG', 1, 1, NULL),
 (11, 'COTRINA37', '$2y$10$uB5zvGMgT.GUPiXk5VA94uBlXxFZ.89Y/wjsFCHtSV0nJx75ugoNm', 1, 1, NULL),
-(10, 'SONIA45', '$2y$10$tfTcJ1mLAy0mkDPgawSzDO.s4WwP9mXtKaOG.0jQrqxbODTG3XwAS', 1, 1, NULL);
+(10, 'SONIA45', '$2y$10$tfTcJ1mLAy0mkDPgawSzDO.s4WwP9mXtKaOG.0jQrqxbODTG3XwAS', 1, 1, NULL),
+(55, 'yuyu', '$2y$10$Udq3uTKf5sapD/ixL0n/CufD1Xfi45Ad3TOsXGPk4jkyAVh/SMtKK', 1, 3, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -419,12 +478,24 @@ ALTER TABLE `creditos`
   ADD KEY `creditos_idusuario_foreign` (`idusuario`);
 
 --
+-- Indices de la tabla `creditos_bitacora`
+--
+ALTER TABLE `creditos_bitacora`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `cuotas`
 --
 ALTER TABLE `cuotas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `cuotas_idusuario_foreign` (`idusuario`),
   ADD KEY `cuotas_idcredito_foreign` (`idcredito`);
+
+--
+-- Indices de la tabla `cuotas_bitacora`
+--
+ALTER TABLE `cuotas_bitacora`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `migrations`
@@ -478,10 +549,22 @@ ALTER TABLE `creditos`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
+-- AUTO_INCREMENT de la tabla `creditos_bitacora`
+--
+ALTER TABLE `creditos_bitacora`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de la tabla `cuotas`
 --
 ALTER TABLE `cuotas`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=223;
+
+--
+-- AUTO_INCREMENT de la tabla `cuotas_bitacora`
+--
+ALTER TABLE `cuotas_bitacora`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
@@ -493,7 +576,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de la tabla `personas`
 --
 ALTER TABLE `personas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
