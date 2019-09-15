3bbe481a9aaf910d424b822bb6fb3cca5271165d
joseluisrq <sistemas@cersa.org.pe>
2019-09-14 21:18:34 -0500

    js

--- public/js/app.js
+++ public/js/app.js
@@ -76333,7 +76333,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp2\htdocs\apt\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\apt\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
