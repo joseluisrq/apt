5544b2e2324761589534cc41cac73caa3a65f372
Jose Luis Ramirez <joseramirezq.info@gmail.com>
2019-09-24 18:54:30 -0500

    detalle credito

--- resources/views/pdf/detallecredito.blade.php
+++ resources/views/pdf/detallecredito.blade.php
@@ -6,42 +6,78 @@
   
     <title>Reporte de Credito</title>
     <style>
-           body {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
-        background-color: #FAFAFA;
-        font: 12pt "Tahoma";
-    }
-    * {
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-    }
-    .book{
-        width: 40%;
-    }
-    .page {
-        width: 210mm;
-        min-height: 297mm;
-        padding: 20mm;
-        margin: 10mm auto;
-        border: 1px #D3D3D3 solid;
-        border-radius: 5px;
-        background: white;
-        box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-    }
-    .subpage {
-        padding: 1cm;
-        border: 5px red solid;
-        height: 257mm;
-        outline: 2cm #FFEAEA solid;
-    }
-    
-    @page {
-        size: A4;
-        margin: 0;
-    }
+     body {
+            margin: 20px;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+            font-size: 0.875rem;
+            font-weight: normal;
+            line-height: 1.5;
+            color: #151b1e;           
+        }
+        .table {
+            display: table;
+            width: 100%;
+            max-width: 100%;
+            margin-bottom: 1rem;
+            background-color: transparent;
+            border-collapse: collapse;
+        }
+        .table-bordered {
+            border: 1px solid #c2cfd6;
+        }
+        thead {
+            display: table-header-group;
+            vertical-align: middle;
+            border-color: inherit;
+        }
+        tr {
+            display: table-row;
+            vertical-align: inherit;
+            border-color: inherit;
+        }
+        .table th, .table td {
+            padding: 0.75rem;
+            vertical-align: top;
+            border-top: 1px solid #c2cfd6;
+        }
+        .table thead th {
+            vertical-align: bottom;
+            border-bottom: 2px solid #c2cfd6;
+        }
+        .table-bordered thead th, .table-bordered thead td {
+            border-bottom-width: 2px;
+        }
+        .table-bordered th, .table-bordered td {
+            border: 1px solid #c2cfd6;
+        }
+        th, td {
+            display: table-cell;
+            vertical-align: inherit;
+        }
+        th {
+            font-weight: bold;
+            text-align: -internal-center;
+            text-align: left;
+        }
+        tbody {
+            display: table-row-group;
+            vertical-align: middle;
+            border-color: inherit;
+        }
+        tr {
+            display: table-row;
+            vertical-align: inherit;
+            border-color: inherit;
+        }
+        .table-striped tbody tr:nth-of-type(odd) {
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+        .izquierda{
+            float:left;
+        }
+        .derecha{
+            float:right;
+        }
     table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
@@ -64,64 +100,112 @@ td, th {
             <p>
                 ID PRESTAMO :  {{$c->numeroprestamo}}<br><br>
                 ID CLIENTE:  {{$c->dni}}<br><br>
-                ID KIVA:  {{$c->idkiva}}<br><br>
+                ID KIVA:  {{$c->idkiva}}<br><
             </p>
              <p>
                   Yo,  {{$c->nombre}}  {{$c->apellidopaterno}} {{$c->apellidomaterno}} con DNI  N° 
-                   {{$c->dni}};entiendo y estoy de acuerdo realizar los pagos en acuerdo con los
+                   {{$c->dni}}; entiendo y estoy de acuerdo realizar los pagos en acuerdo con los
                     términos y condiciones 
             </p>
-            <p> Firma de Acreditado ______________________________ Fecha {{date('d/m/Y')}}</p>
+            <p> Firma de Acreditado ______________________________  &nbsp;&nbsp;&nbsp; Fecha {{date('d/m/Y')}}</p>
             <p> [Representante de APT del Norte] </p>
-            <p>Firma ______________________________ Fecha {{date('d/m/Y')}} </p>
+            <p>Firma ______________________________  &nbsp;&nbsp;&nbsp; Fecha {{date('d/m/Y')}} </p>
             
 
             <h4> I.Términos de Préstamo</h4>
-            <table>
+            <table  style="width:100%">
                 <tr>
-                    <td>Monto de Prestamo y moneda</td>
-                    <td>$ {{$c->montodesembolsado}}</td>
-                    <td>Fecha de desembolso</td>
-                    <td>{{$c->fechadesembolso}}</td>
+                    <td style="width:35%">Monto de Prestamo y moneda</td>
+                    <td style="width:15%">$ {{$c->montodesembolsado}}</td>
+                    <td style="width:35%">Fecha de desembolso</td>
+                    <td style="width:15%">{{$c->fechadesembolso}}</td>
                 </tr>
                 <tr>
-                    <td>Tasa de Interes (Especificar si es Fija/Sobre Saldo/No aplicable)</td>
-                    <td>Fija {{$c->tasa}} % anual</td>
-                    <td>Fecha de vencimiento</td>
-                    <td></td>
+                    <td style="width:35%">Tasa de Interes (Especificar si es Fija/Sobre Saldo/No aplicable)</td>
+                    <td style="width:15%">Fija {{$c->tasa}} % anual</td>
+                    <td style="width:35%">Fecha de vencimiento</td>
+                    <td style="width:15%"></td>
                 </tr>
 
                 <tr>  
-                    <td>Frecuencia de Pagos (Semana, Mensual,etc)</td>
-                    <td>{{$c->periodo}}</td>
-                    <td>Cargos y otroscostos</td>
-                    <td></td>                         
+                    <td style="width:35%">Frecuencia de Pagos (Semana, Mensual,etc)</td>
+                    <td style="width:15%">
+                    <?php
+                    if($c->periodo==1)echo "Mensual";
+                    elseif($c->periodo==2)echo "Bimestral";
+                    elseif($c->periodo==3)echo "Trimestral";
+                    elseif($c->periodo==6)echo "Semestral";
+                    elseif($c->periodo==12)echo "Anual";
+                    ?>
+                   
+                
+                </td>
+                    <td style="width:35%">Cargos y otroscostos</td>
+                    <td style="width:15%">
+                    <?php
+                        echo $meses=($c->numerocuotas)*($c->periodo);
+
+                     ?>
+                
+                    </td>                         
                     
                 </tr>
             </table>
 
 
             <h4>II.Plan de Pagos </h4>
-                <table>
+                <table style="width:100%">
                     <tr>
-                        <th>Fecha de Pago</th>
-                        <th>Saldo Pendiente</th>
-                        <th>Principal</th>
-                        <th>Interes</th>
-                        <th>Otros Costos(Comisiones,seguros,G.B.)</th>
-                        <th>Pago total</th>
+                       <td style="width:17%">Fecha de Pago</th>
+                       <td style="width:17%">Saldo Pendiente</th>
+                       <td style="width:17%">Principal</th>
+                       <td style="width:17%">Interes</th>
+                       <td style="width:17%">Otros Costos</th>
+                       <td style="width:15%">Pago total</th>
                     </tr>
+                    <?php 
+                        
+                        $sumaotros=0; ?>
                     @foreach ($cuotas as $cuot)
                    
                     <tr>
-                        <td>{{$cuot->fechapago}}</td>
-                        <td>{{$cuot->saldopendiente}}</td>
-                        <td>{{$cuot->monto}}</td>
-                        <td>{{$c->tasa}}</td>
-                        <td>{{$cuot->otroscostos}}</td>
-                        <td>{{$cuot->monto}}</td>
+                        <td style="width:17%">{{$cuot->fechapago}}</td>
+                        <td style="width:17%">{{round($cuot->saldopendiente,2)}}</td>
+                        <td style="width:17%">{{
+                            round(
+                            $cuot->monto
+                            ,2)
+                        }}</td>
+                        <td style="width:17%">{{
+                            round(
+                            ($c->montodesembolsado*($c->tasa / 100)) / $c->numerocuotas
+                            ,2)
+                        
+                        }}</td>
+                        <td style="width:17%">{{$cuot->otroscostos}}</td>
+                        <td style="width:15%">{{round(($cuot->monto)+($cuot->otroscostos)+( ($c->montodesembolsado*($c->tasa / 100)) / $c->numerocuotas),2)}}</td>
+                        <?php 
+                        
+                        $sumaotros=($cuot->otroscostos)+$sumaotros?>
                     </tr>
                     @endforeach
+                    <td style="width:17%">TOTAL</td>
+                        <td style="width:17%">0</td>
+                        <td style="width:17%">{{$c->montodesembolsado}}</td>
+                        <td style="width:17%">{{
+                            round(
+                            $c->montodesembolsado*($c->tasa / 100)
+                            ,2)
+                        
+                        }}</td>
+                        <td style="width:17%"><?php echo $sumaotros?></td>
+                        <td style="width:15%">{{
+                            round(
+                            $c->montodesembolsado+$sumaotros+  ($c->montodesembolsado*($c->tasa / 100))
+                            ,2)
+                        
+                        }}</td>
+                    </tr>
 
                 </table>
             <h4>
